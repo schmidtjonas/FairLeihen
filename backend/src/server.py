717941c2f81a5ca126f417e441a0c84f5d2fa68c
Jonas Schmidt <mail@schmidt-jonas.de>
2019-04-19 01:37:43 +0200

    added very basic flask server testing

---
+++ backend/src/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, abort, jsonify
+
+app = Flask(__name__)
+
+users = []
+
+@app.route('/')
+def hello():
+    return 'hello\n'
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username', None)
+    user_taken = username in users
+
+    if username is None or user_taken:
+        abort(401)
+    else:
+        users.append(username)
+        return jsonify({
+            'status': 'OK',
+            'message': 'Successfully logged in'
+            })
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
