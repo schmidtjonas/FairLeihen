ab0e9a65fd68ff847e9d516c98dfa3471ae815ee
Jonas Schmidt <mail@schmidt-jonas.de>
2019-04-19 15:23:18 +0200

    register server

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,8 +1,32 @@
 from flask import Flask, request, abort, jsonify
 
+import hashlib, binascii, os
+ 
+def hash_password(password):
+    """Hash a password for storing."""
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), 
+                                salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash)
+    return (salt + pwdhash).decode('ascii')
+ 
+def verify_password(stored_password, provided_password):
+    """Verify a stored password against one provided by user"""
+    salt = stored_password[:64]
+    stored_password = stored_password[64:]
+    pwdhash = hashlib.pbkdf2_hmac('sha512', 
+                                  provided_password.encode('utf-8'), 
+                                  salt.encode('ascii'), 
+                                  100000)
+    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+    return pwdhash == stored_password
+
+
+
+
 app = Flask(__name__)
 
-users = []
+users = {}
 
 @app.route('/')
 def hello():
@@ -11,17 +35,39 @@ def hello():
 @app.route('/login', methods=['POST'])
 def login():
     username = request.json.get('username', None)
-    user_taken = username in users
+    password = request.json.get('password', None)
 
-    if username is None or user_taken:
+    user_exists = username in users.keys()
+
+    if username is None or not user_exists:
         abort(401)
-    else:
-        users.append(username)
+    
+    elif verify_password(users[username]['password'], password):
         return jsonify({
             'status': 'OK',
             'message': 'Successfully logged in'
             })
+    abort(403)
 
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    email = request.json.get('email', None)
+
+    if username in users.keys() or username is None or email is None or password is None:
+        abort(400)
+
+    else:
+        users[username] = {
+            'password': hash_password(password),
+            'email': email
+        }
+        return jsonify({
+            'status': 'OK',
+            'message': 'Successfully registered'
+            })
 
 
 if __name__ == '__main__':
