021d1c64d556dae333472d14469605be3a9acee1
Jonas Schmidt <mail@schmidt-jonas.de>
2019-04-22 17:44:23 +0200

    standort und image

--- frontend/src/components/screens/LinksScreen.js
+++ frontend/src/components/screens/LinksScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
 import MapView from 'react-native-maps';
+import { Constants, Location, Permissions } from 'expo';
+
 
 //In diesem Screen ist erstmal die Map
 //Ich war zu faul alles umzubenennen aber das sollten wir dann tun
@@ -14,42 +16,100 @@ export default class LinksScreen extends React.Component {
     title: 'Map',
   };
 
-  render() {
-    return (
-        <MapView style={styles.map}
+state = {
+  mapRegion: null,
+  hasLocationPermissions: false,
+  locationResult: null
+};
+
+componentDidMount() {
+  this._getLocationAsync();
+}
+
+_handleMapRegionChange = mapRegion => {
+  this.setState({ mapRegion });
+};
+
+_getLocationAsync = async () => {
+ let { status } = await Permissions.askAsync(Permissions.LOCATION);
+ if (status !== 'granted') {
+   this.setState({
+     locationResult: 'Permission to access location was denied',
+   });
+ } else {
+   this.setState({ hasLocationPermissions: true });
+ }
+
+ let location = await Location.getCurrentPositionAsync({});
+ this.setState({ locationResult: JSON.stringify(location) });
+ 
+ // Center the map on the location we just fetched.
+  this.setState({mapRegion: { latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }});
+};
+
+render() {
+  return (
+    <View style={styles.container}>
+      
+      {
+        //this.state.locationResult === null ?
+        //<Text>Finding your current location...</Text> :
+        //this.state.hasLocationPermissions === false ?
+          //<Text>Location permissions are not granted.</Text> :
+          //this.state.mapRegion === null ?
+          //<Text>Map region doesn't exist.</Text> :
+          <MapView style={styles.map}
           provider={'google'}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
->
+          
+          initialRegion={this.state.mapRegion}
+          onRegionChange={this._handleMapRegionChange}
+          showsUserLocation={true}
+          showsMyLocationButton={true}>
           <MapView.Marker 
-            coordinate={{latitude: 37.78825,
-            longitude: -122.4324}}
+            coordinate={{latitude: 52.393442,
+            longitude: 13.131462}}
             title={"Mark"}
             description={"Ich bin Mark der Marker"}
-            image={require('../../assets/images/bohrmaschine.png')}
-            onLoad={() => this.forceUpdate()} //sonst lädt das Bild nicht und es lädt nur das häßliche Bild was mega weird ist deshalb erstmal so zum testen
-         />
+            anchor={{ x:0.5, y:0.5 }}>
+          <Image
+            source={require('../../assets/images/b2.png')}
+            style={styles.bohrer}
+            />
+          </MapView.Marker>
        
         </MapView>
-
-    );
-  }
+      }
+      
+      
+    </View>
+      
+  );
+}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-
-  map: {
-    top: 0,
-    left: 0,
-    ...StyleSheet.absoluteFillObject,
-  },
+container: {
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+  paddingTop: Constants.statusBarHeight,
+  backgroundColor: '#ecf0f1',
+},
+paragraph: {
+  margin: 24,
+  fontSize: 18,
+  fontWeight: 'bold',
+  textAlign: 'center',
+  color: '#34495e',
+},
+map: {
+  top: 0,
+  left: 0,
+  ...StyleSheet.absoluteFillObject,
+},
+bohrer: {
+  width: 50,
+  height: 50,
+}
 });
+
